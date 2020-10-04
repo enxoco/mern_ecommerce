9cff778510e13ce27a4196cd33f4519539dfc80c
Mike Conrad <mike@enxo.co>
2020-10-04 15:50:10 -0400

    Hooked up backend to mongo, added api routes and redux

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,27 @@
 import express from 'express'
-import products from './data/products.js'
+import connectDB from './config/db.js'
+import colors from 'colors'
 const app = express()
 import dotenv from 'dotenv'
-
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 dotenv.config()
+
+connectDB()
+
 app.get('/', (req, res) => {
   res.send('API is running')
 })
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use('/api/products', productRoutes)
 
+//Register middleware for error/route handling
+app.use(notFound)
+app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.green.bold
+  )
 )
