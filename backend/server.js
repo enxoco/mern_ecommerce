ede9720e576b7657888670fe03695ee7cf0f11cc
Mike Conrad <mike@enxo.co>
2020-10-06 09:26:46 -0400

    Began working on user auth routes

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ import express from 'express'
 import connectDB from './config/db.js'
 import colors from 'colors'
 const app = express()
+
+app.use(express.json())
 import dotenv from 'dotenv'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 dotenv.config()
 
@@ -14,6 +17,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 //Register middleware for error/route handling
 app.use(notFound)
